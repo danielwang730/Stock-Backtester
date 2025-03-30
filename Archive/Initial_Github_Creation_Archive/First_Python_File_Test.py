e980b4ec98d274697316fcb8a04016282b04884c
danielwang730 <daniel.wang730@gmail.com>
2025-03-30 16:58:33 -0400

    Copying backtesting notebooks over from previous repository

---
+++ Archive/Initial_Github_Creation_Archive/First_Python_File_Test.py
@@ -0,0 +1,5 @@
+print('First python file test')
+print('Hello World') # addition from Hayes to test git and github
+print('Addition received') # Confirming addition from Hayes
+
+print('Adding branch for testing') # Adding a branch
